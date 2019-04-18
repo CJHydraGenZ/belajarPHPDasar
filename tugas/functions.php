2a6484579fb329f5872008dd05efb98a20e743a7
CJHydraGenZ <cahyasaga321@gmail.com>
2019-04-18 16:34:03 +0800

    update

--- tugas/functions.php
+++ tugas/functions.php
@@ -1,3 +1,18 @@
 <?php
 
 $conn = mysqli_connect("localhost", "root", "", "db_stikom");
+
+
+
+function ubah($data)
+{
+    global $conn;
+    $nim = $data["nim"];
+    $Unama = $data["Unama"];
+    $Ualamat = $data["Ualamat"];
+
+    $query = "UPDATE tb_mahasiswa SET nim = '$Unama', alamat = '$Ualamat' WHERE nim = $nim";
+
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
--- tugas/index.php
+++ tugas/index.php
@@ -1,12 +1,14 @@
 <?php
 
 require 'functions.php';
+
+$id = $_GET["nim"];
 $query = "SELECT * FROM tb_mahasiswa";
 
 $result = mysqli_query($conn, $query);
+if (isset($_POST["Ukirim"]))
 
-
-?>
+    ?>
 
 <!doctype html>
 <html lang="en">
@@ -46,9 +48,38 @@ $result = mysqli_query($conn, $query);
                 <td><?= $row["nama"];   ?></td>
                 <td><?= $row["alamat"];   ?></td>
                 <td>
-                    <a class="btn btn-primary" data-toggle="collapse" href="index.php" role="button" aria-expanded="false" aria-controls="collapseExample" onclick="return alert('Masih Maintenance')">
+
+                    <a class="btn btn-primary" href="index.php" role="button" aria-controls="collapseExample" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">
                         Ubah
                     </a>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Ubah data</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="recipient-name" class="col-form-label">Nama:</label>
+                                            <input type="text" class="form-control" id="recipient-name" name="Unama">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="col-form-label">Alamat:</label>
+                                            <textarea class="form-control" id="message-text" name="Ualamat"></textarea>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" name="Ukirim">Ubah</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <a class="btn btn-primary" href="hapus.php?nim=<?= $row["nim"];   ?> " role="button" aria-controls="collapseExample" onclick="return confirm('Yakin?');">
                         Hapus
                     </a>
